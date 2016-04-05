93c62e0d27a49d292cb1e2d5316473d4cc74df38
DannChe <dann.ua@mail.ru>
2016-04-05 23:12:06 +0300

    Field added

---
+++ src/Field.java
@@ -0,0 +1,34 @@
+import java.util.Arrays;
+
+class Field {
+    private char[][] arr = new char[20][20];
+
+    void fill(){
+        for (int i = 0; i < 20; i++) {
+            for (int j = 0; j < 20; j++) {
+                if (i == 0 || i == 19) {
+                    arr[i][j] = '#';
+                }else{
+                    if (j == 0 || j == 19){
+                        arr[i][j] = '#';
+                    }else{
+                        arr[i][j] = ' ';
+                    }
+                }
+            }
+        }
+    }
+
+    void draw() {
+        /*for (int i = 0; i < 10; i++) {
+            System.out.println(Arrays.toString(arr[i]));
+        }*/
+
+        for (int i = 0; i < 20; i++) {
+            for (int j = 0; j < 20; j++) {
+                System.out.print(arr[i][j]);
+            }
+            System.out.println();
+        }
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,7 @@
-/**
- * Created by Denis on 4/5/2016.
- */
 public class Main {
     public static void main(String[] args) {
-        System.out.printf("%s, %s", "Hello", "World!");
+        Field field = new Field();
+        field.fill();
+        field.draw();
     }
 }
