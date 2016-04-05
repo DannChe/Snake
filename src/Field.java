1218100650deb5a95c6db72acfe4250b970eab09
DannChe <dann.ua@mail.ru>
2016-04-05 23:33:12 +0300

    Field finished

--- src/Field.java
+++ src/Field.java
@@ -20,15 +20,15 @@ class Field {
     }
 
     void draw() {
-        /*for (int i = 0; i < 10; i++) {
-            System.out.println(Arrays.toString(arr[i]));
-        }*/
-
         for (int i = 0; i < 20; i++) {
+            System.out.println(Arrays.toString(arr[i]));
+        }
+
+/*        for (int i = 0; i < 20; i++) {
             for (int j = 0; j < 20; j++) {
                 System.out.print(arr[i][j]);
             }
             System.out.println();
-        }
+        }*/
     }
 }
